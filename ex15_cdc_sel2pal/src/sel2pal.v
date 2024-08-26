07e8ca7f2c0c2687c129e9789b2bda7444345486
Chen-gz233 <1738332914@qq.com>
2024-08-26 11:21:33 +0800

    add code

---
+++ ex15_cdc_sel2pal/src/sel2pal.v
@@ -0,0 +1,57 @@
+`timescale 1ns/1ns
+
+module sel2pal( //fast to slow 
+    input clka    ,//fast
+    input wra_n   ,
+    input rst_n   ,
+    input din     ,
+
+    input clkb    ,//slow
+    output wrb     ,
+    output [7:0] db_out
+);
+
+    reg [7:0] din_pal ;  //串行 转 并行
+    reg [7:0] db_buf  ;
+
+    reg [2:0] wra_b ;
+    reg wrb_reg ;
+
+//串行数据转并行数据
+always @(posedge clka or negedge rst_n) begin
+    if(!rst_n)begin
+        din_pal <= 8'd0 ;
+    end else if (!wra_n) begin
+        din_pal <= {din_pal[6:0],din};
+    end esle begin
+        din_pal <= 8'd0 ;
+    end 
+end 
+
+//使能信号 跨时钟域处理
+always @(posedge clkb or negedge rst_n) begin
+    if(!rst_n)begin
+        wra_b <= 3'b0 ;
+    end else begin
+        wra_b <= {wra_b[1:0],wra_n} ;
+    end 
+end
+
+// 使n能信号 使 寄存器数据交换
+always @(posedge clkb or negedge rst_n) begin
+    if(!rst_n)begin
+        db_buf <= 8'd0 ;
+        wrb_reg <= 1'b0 ;
+    end else if (!wra_b[2] & wra_b[1])begin
+        db_buf <= din_pal;
+        wrb_reg <= 1'b1 ;
+    end else begin
+        db_buf <= 8'd0 ;
+        wrb_reg <= 1'b0 ;
+    end 
+end
+    
+
+    assign  wrb   =   wrb_reg ;
+    assign db_out =   db_buf  ;
+endmodule
---
+++ ex15_cdc_sel2pal/tb/sel2pal_tb.v
@@ -0,0 +1,71 @@
+module tb;
+reg clka  ; 
+reg clkb  ; 
+reg rst_n ; 
+reg din   ; 
+reg wra_n ; 
+reg wrb   ; 
+reg [7:0]db_out;
+
+reg [3:0]cnt;
+
+always@(posedge clka)begin
+din <= #1 {$random}%2;
+cnt <= #1 cnt+1;
+end
+
+always #5 clka = !clka;
+always #10 clkb = !clkb;
+
+always@(*)begin
+if(cnt==0)
+    wra_n=0;
+else if(cnt==8)
+    wra_n=1;
+end
+
+initial begin
+clka  = 0;
+clkb  = 0;
+rst_n = 1;
+cnt   = 1;
+
+#10
+rst_n = 0;
+
+#20
+rst_n = 1;
+
+#2000
+$finish;
+end
+
+ser_pal ser_pal_u(
+    .clka(clka),
+    .clkb(clkb),
+    .rst_n(rst_n),
+    .din(din),
+    .wra_n(wra_n),
+    .wrb(wrb),
+    .db_out(db_out)
+
+);
+/* 
+
+module sel2pal( //fast to slow 
+    input clka    ,//fast
+    input wra_n   ,
+    input rst_n   ,
+    input din     ,
+
+    input clkb    ,//slow
+    output wrb     ,
+    output [7:0] db_out
+);
+*/
+initial	begin
+	    $fsdbDumpfile("tb.fsdb");	    
+        $fsdbDumpvars;
+end
+
+endmodule
